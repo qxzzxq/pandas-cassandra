62b414e99ac04da7d808125d39cad34dd97940d5
Qin Xuzhou <xuzhou.qin@jcdecaux.com>
2018-02-20 17:10:25 +0100

    add CassandraDataFrame.from_cassandra()
    Signed-off-by: Qin Xuzhou <xuzhou.qin@jcdecaux.com>

--- pandra/cassandradataframe.py
+++ pandra/cassandradataframe.py
@@ -13,6 +13,9 @@ class CassandraDataFrame(pd.DataFrame):
     Add a new methods to pandas DataFrame object.
     """
 
+    # Add cassandra session to CassandraDataFrame
+    _metadata = ['cassandra_session']
+
     @property
     def _constructor(self):
         return CassandraDataFrame
@@ -35,7 +38,6 @@ class CassandraDataFrame(pd.DataFrame):
         """
         if isinstance(types, dict):
             return types
-
         else:
             column_names = self.columns
             return dict(zip(column_names, types))
@@ -66,32 +68,32 @@ class CassandraDataFrame(pd.DataFrame):
 
         # Create a cassandra connector
         connector = type(table_name, (cql_connector.Model,), data_types)
-
         __create_table = create_table
-
         for index, row in self.iterrows():
             row_to_insert = connector(**row.to_dict())
 
             if __create_table:
                 cql_create = row_to_insert.create()
-
                 try:
                     cassandra_session.execute(cql_create)
                 except cassandra.AlreadyExists:
                     logging.WARN('Table {} already exists.'.format(table_name))
-
                 __create_table = False
 
             cql_insert, values = row_to_insert.insert()
-
             try:
                 if debug:
                     print(cql_insert, values)
                 else:
                     cassandra_session.execute(cql_insert, values)
+
             except Exception as e:
                 logging.warning(e)
 
     @classmethod
-    def from_s3(cls, data):
-        pass
+    def from_cassandra(cls, cassandra_session, cql_command, async=False):
+        # TODO: add async method
+        # TODO: auto detect column type
+        raw_query = cassandra_session.execute(cql_command)
+        x = list(raw_query)[:]
+        return pd.DataFrame(x)
